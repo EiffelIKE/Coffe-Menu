0c1687e700b9ff63d4e89014b8ca43da8211003e
EiffelIKE <jctrujillo759@gmail.com>
2022-08-09 23:04:24 -0700

    Welcome page created, also added two buttons with bootstrap styles with react router's useNavigate()  hooks

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Menu from './pages/Menu'
+import Welcome from './pages/Welcome'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 
 const App = () => {
@@ -7,6 +8,7 @@ const App = () => {
     <BrowserRouter>
       <Routes>
       <Route path='/' element= {<Navigate to= '/Welcome' /> } />
+      <Route path='/Welcome' element= {<Welcome/>}/>
       <Route path= '/Menu' element= {<Menu/>} />
       {/* <Route path= '*' element={<NotFound/>} /> */}
       </Routes>
